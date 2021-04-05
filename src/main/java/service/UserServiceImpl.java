cdd39a016f0518e298f24679463bcfe8b77660dc
Buffer <1226098687@qq.com>
2021-04-05 19:41:32 +0800

    第四次提交，优化了UserController、UserService、UserServiceImpl的相关相关内容(这三个可以完事了)

--- src/main/java/service/UserServiceImpl.java
+++ src/main/java/service/UserServiceImpl.java
@@ -2,34 +2,28 @@ package service;
 
 import java.util.List;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 
 import pojo.*;
 import dao.UserMapper;
 
 @Service
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
-	@Autowired
-	private UserMapper userMapper;
+    @Autowired
+    private UserMapper userMapper;
 
-	@Override
-	public List<User> userList() throws Exception {
-		// TODO Auto-generated method stub
-		UserExample example=new UserExample();
-		List<User> list=userMapper.selectByExample(example);
-		System.out.println("123"+list);
-		return list;
-	}
+    @Override
+    public List<User> userList() throws Exception {
+        UserExample example = new UserExample();
+        List<User> list = userMapper.selectByExample(example);
+        System.out.println("123" + list);
+        return list;
+    }
 
-	@Override
-	public User login(User user) throws Exception {
-		User user1=userMapper.selectByUser(user);
-		return user1;
-	}
-
-	
+    @Override
+    public User login(User user) throws Exception {
+        return userMapper.selectByUser(user);
+    }
 }
